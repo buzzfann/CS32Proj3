e288512f597ecf50c6c6c304d870a380210b471e
buzzfann <78663982+buzzfann@users.noreply.github.com>
2021-02-18 21:36:15 -0800

    Create Actor.cpp

---
+++ Actor.cpp
@@ -0,0 +1,11 @@
+#include "Actor.h"
+#include "StudentWorld.h"
+#include <vector>
+#include "GraphObject.h"
+#include <cstdlib>
+// Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+void GhostRacer::doSomething()
+{
+    
+}
