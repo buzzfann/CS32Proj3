5f7a74ad2ebf126d687ab2cd1b7c2d058ebec799
buzzfann <78663982+buzzfann@users.noreply.github.com>
2021-02-18 21:40:49 -0800

    Create StudentWorld.h

---
+++ Part1/StudentWorld.h
@@ -0,0 +1,27 @@
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
+
+#include "GameWorld.h"
+#include <string>
+#include <vector>
+#include "Actor.h"
+
+
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class StudentWorld : public GameWorld
+{
+public:
+    StudentWorld(std::string assetPath);
+    virtual int init();
+    virtual int move();
+    virtual void cleanUp();
+    ~StudentWorld();
+//    bool checkPosition(double x, double y);
+
+private:
+    GhostRacer* m_ghost;
+    std::vector<Actor*> actors;
+};
+
+#endif // STUDENTWORLD_H_
