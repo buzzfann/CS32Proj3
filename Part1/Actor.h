425669ab67762bcc1eb0a5dd460ab3537d196ca6
buzzfann <78663982+buzzfann@users.noreply.github.com>
2021-02-19 11:58:14 -0800

    Update Actor.h

--- Part1/Actor.h
+++ Part1/Actor.h
@@ -18,22 +18,40 @@ const double INITIAL_SPEED = 0.0;
 const int INITIAL_SPRAY = 10;
 const int INITIAL_HEALTH = 100;
 
+//Border Lines
+const int borderDirection = 0;
+const int borderDepth = 2;
+const double borderSize = 2.0;
+const int borderYSpeed = -4;
+const int borderXSpeed = 0;
+
+const int NEW_BORDER_Y = VIEW_HEIGHT - SPRITE_HEIGHT;
+
 
 
 class Actor : public GraphObject
 {
 public:
-    Actor (int imageID, double startX, double startY, int dir, double size, int depth, int health, int spray, StudentWorld* world) :
+    Actor (int imageID, double startX, double startY, int dir, double size, int depth, bool alive, bool collideable, StudentWorld* world) :
             GraphObject(imageID, startX, startY, dir, size, depth),
-            m_world(world), isAlive(true) {};
-    bool retAlive() {return isAlive;};
+            m_world(world), m_alive(alive), m_collideable(collideable) {};
+    bool retAlive() {return m_alive;};
+    
     virtual void doSomething() = 0;
-    StudentWorld* getWorld();
+    StudentWorld* getWorld() {return m_world;};
     int getImageID();
+    void setSpeed(int xs, int ys);
+    int getVertSpeed(){return m_ySpeed;}
+    int getHorizSpeed(){return m_xSpeed;}
+    bool isAlive(){return m_alive;}
+    void setAlive(bool set){m_alive = set;}
 //    virtual ~Actor();
     
 private:
-    bool isAlive;
+    bool m_alive;
+    bool m_collideable;
+    int m_xSpeed;
+    int m_ySpeed;
     StudentWorld* m_world;
 };
 
@@ -41,27 +59,27 @@ class GhostRacer : public Actor
 {
 public:
     GhostRacer(int startX, int startY, StudentWorld* world)
-    : Actor(IID_GHOST_RACER, INITIAL_X, INITIAL_Y, INITIAL_DIRECTION, SIZE, DEPTH, INITIAL_HEALTH, INITIAL_SPRAY, world), alive(true)
+    : Actor(IID_GHOST_RACER, INITIAL_X, INITIAL_Y, INITIAL_DIRECTION, SIZE, DEPTH, true, true, world), m_health(INITIAL_HEALTH), m_spray(INITIAL_SPRAY)
     {};
     virtual void doSomething();
+    void setHealth(int amount){m_health = m_health + amount;}
 private:
-    bool alive;
+    int m_health;
+    int m_spray;
 };
 
 class BorderLines : public Actor
 {
 public:
-    
+    BorderLines (int imageID, double startX, double startY, StudentWorld* world) :
+    Actor (imageID, startX, startY, borderDirection, borderSize, borderDepth, true, false, world) {
+        setSpeed(-4, 0);
+//        m_ghost =
+    };
     virtual void doSomething();
-    int new_border_y = VIEW_HEIGHT - SPRITE_HEIGHT;
-//    int delta_y = new_border_y -
-//    if (delta_y >= SPRITE_HEIGHT)
-//    {
-//
-//    }
     
 private:
-    
+//    GhostRacer* m_ghost;
 };
 
 //class HumanPed : public Actor
